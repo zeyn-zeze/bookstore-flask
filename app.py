6d29fd9a92eac06efa875d5d57bd89d486adf78e
Zeynep Ö. <116917341+zeyn-zeze@users.noreply.github.com>
2025-04-30 22:26:06 +0200

    CartItem migration process

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from extensions import db,migrate
 from controllers.auth_controller import auth_bp
 from controllers.book_controller import book_bp
 from controllers.user_controller import user_bp
+from controllers.cart_controller import cart_bp
+
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(book_bp)
     app.register_blueprint(user_bp)
+    app.register_blueprint(cart_bp)
 
     return app
 
